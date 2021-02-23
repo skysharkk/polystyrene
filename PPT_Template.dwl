78ad069e49ef02a58bc695fb4c497c13650da39b
skysharkk <sky.light.blr@gmail.com>
2021-02-23 13:39:54 +0300

    implement get selection function

---
+++ PPT_Template.dwl
@@ -0,0 +1,3 @@
+osadchiy
+KTO-OSADCHY 
+23 февраля 2021 г.  12:57:07
--- polystyrene.py
+++ polystyrene.py
@@ -1,18 +1,43 @@
 import win32com.client
 import win32gui
 import win32process
+import logging
 
 acad = win32com.client.Dispatch("AutoCAD.Application")
-
-# shell = win32com.client.Dispatch("WScript.Shell") #windows scripts
+shell = win32com.client.Dispatch("WScript.Shell")  # windows scripts
 doc = acad.ActiveDocument
+logger = logging.getLogger(__name__)
 
-# shell.AppActivate(acad.Caption) #change focus to autocad
-# shell.SendKeys("{ENTER}") #press enter key
 
-doc.Utility.Prompt("Press enter!")
-doc.SendCommand("BOUNDARY")
+# def create_boundary():
+#     doc.Utility.Prompt(
+#         "Нажмите ENTER или правую кнопку мыши для образования контура\n")
+#     # doc.SendCommand("BOUNDARY") #for english version
+#     doc.SendCommand("КОНТУР")
+#     # shell.SendKeys("{ENTER}")  # press enter key
 
-# entity = doc.Utility.GetEntity()
 
-# print(entity[0].Area)
+def get_selection(doc, text="Выберете объект"):
+    doc.Utility.prompt(text)
+    try:
+        doc.SelectionSets.Item("SS1").Delete()
+    except Exception:
+        logger.debug('Delete selection failed')
+
+    selection = doc.SelectionSets.Add('SS1')
+    selection.SelectOnScreen()
+    return selection
+
+
+def main():
+    shell.AppActivate(acad.Caption)  # change focus to autocad
+    # create_boundary()
+    doc.Utility.Prompt("Выберите образованный контур\n")
+    selection = get_selection(doc, text="Выберете объект")
+    # selection = doc.SelectionSets.Item('SS1')
+    # entity = doc.Utility.GetEntity()
+    # print(entity[0].Area)
+    print(selection.Item(0).Area)
+
+
+main()
