bd7d6381385821cc12a21ee95ee12b8c8765d47b
skysharkk <sky.light.blr@gmail.com>
2021-04-12 13:12:56 +0300

    feat: implemented get bounding box

---
+++ getboundingbox.py
@@ -0,0 +1,12 @@
+from array import array
+from comtypes.automation import VARIANT
+from ctypes import byref
+
+
+def get_bounding_box(entity):
+    min_point = VARIANT(array("d", array("d", [0, 0, 0])))
+    max_point = VARIANT(array("d", array("d", [0, 0, 0])))
+    ref_min_point = byref(min_point)
+    ref_max_point = byref(max_point)
+    entity.GetBoundingBox(ref_min_point, ref_max_point)
+    return [min_point, max_point]
