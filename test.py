8447c90da5c72b38e36598cde1f35c313435d23d
skysharkk <sky.light.blr@gmail.com>
2021-05-05 16:37:49 +0300

    add reprasitation

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import greedypacker
 from pyautocad import Autocad
 from array import array
 from copy import copy
+from pprint import pprint
 
 
 acad = Autocad(create_if_not_exists=True)
@@ -43,27 +44,47 @@ def draw_rectangle(doc, initial_point, width, height):
 
 
 M = greedypacker.BinManager(
-    2000, 1000, pack_algo='maximal_rectangle', heuristic='bottom_left', rotation=True)
+    20, 10, pack_algo='maximal_rectangle', heuristic='bottom_left', rotation=True)
 
-ITEM = greedypacker.Item(1320, 700)
-ITEM4 = greedypacker.Item(200, 200)
-ITEM2 = greedypacker.Item(1320, 700)
-ITEM3 = greedypacker.Item(1320, 720)
-ITEM5 = greedypacker.Item(1000, 670)
+ITEM = greedypacker.Item(5, 6)
+ITEM4 = greedypacker.Item(3, 2)
+# ITEM2 = greedypacker.Item(1320, 700)
+# ITEM3 = greedypacker.Item(1320, 720)
+# ITEM5 = greedypacker.Item(1000, 670)
 
-M.add_items(ITEM, ITEM2, ITEM3, ITEM4, ITEM5)
+M.add_items(ITEM, ITEM4)
 
 M.execute()
 
-print(M.bins)
 
-initial_x = 0
-initial_y = 0
+def draw_packs():
+    initial_x = 0
+    initial_y = 0
+    for p_bin in M.bins:
+        draw_rectangle(doc, array('d', (initial_x, initial_y, 0)), 20, 10)
+        for item in p_bin.items:
+            draw_rectangle(doc, array('d', (item.x + initial_x, item.y + initial_y, 0)),
+                           item.width, item.height)
+        initial_y += 10
 
-for p_bin in M.bins:
-    draw_rectangle(doc, array('d', (initial_x, initial_y, 0)), 2000, 1000)
-    for item in p_bin.items:
-        print(item)
-        draw_rectangle(doc, array('d', (item.x + initial_x, item.y + initial_y, 0)),
-                       item.width, item.height)
-    initial_y += 1000
+
+def generate_rectangle_matrix(width, height):
+    width_list = [0] * width
+    result = []
+    for _ in range(height):
+        result.append(copy(width_list))
+    return result
+
+
+def draw_in_matrix(container_matrix, elements):
+    pprint(elements)
+    for el in elements:
+        for i in range(el.y, el.height):
+            print(el.x, el.width)
+            container_matrix[i][el.x: el.x + el.width] = [1] * (el.width)
+    return container_matrix
+
+
+mat = draw_in_matrix(generate_rectangle_matrix(20, 10), M.items)
+pprint(mat)
+draw_packs()
